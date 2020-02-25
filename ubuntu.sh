1c1fe2ce89f5580abf4fc6a960d4c8abb36991b2
henriqueyun <henriqueyun@gmail.com>
2020-02-25 02:13:44 -0300

    First commmit, ubuntu.sh done4

---
+++ ubuntu.sh
@@ -0,0 +1,36 @@
+echo "</> Vim & Curl"
+sudo apt install vim curl
+
+echo "</> Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+echo "</> Atom"
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install atom
+
+echo "</> APM Packages"
+apm install language-docker file-icons
+
+echo "</> Docker"
+sudo apt install docker.io
+
+echo "</> Kubernetes CLI Tool (kubectl)"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "</> Minikube"
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+
+echo "</> IntelliJ"
+sudo snap install intellij-idea-community --classic
+
+echo "</> Tweaks"
+sudo apt install gnome-tweaks
